73667681838f6f8f65c2b16fdae9fd686f1ed5f3
fr3ddy <mgoltseva@wallarm.com>
2019-11-05 19:39:08 +0300

    test v.4.20

--- local.sh
+++ local.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-export TOKEN=YOUR WALLARM NODE TOKEN
+export TOKEN=waf1n4cJsQ7dIL7TthDfMueCe/MCSd7LBhCDgLVXvsO1YZ8w59tJUY6hUQbIXSZE
 
-sudo -E docker-compose build
-sudo -E docker-compose up -d dvwa fast selenium
-sudo -E docker-compose run test
-sudo -E docker-compose run --rm -e CI_MODE=testing -e TEST_RUN_URI=http://dvwa:80 fast
-sudo -E docker-compose down
+docker-compose build
+docker-compose up -d dvwa fast 
+docker-compose run --rm -e CI_MODE=testing -e TEST_RUN_URI=http://dvwa:80 -e TEST_RECORD_ID=3219 fast
+docker-compose down
