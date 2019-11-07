3e2e1ca60e00317b23b4865fe8c3780d2300e21d
fr3ddy <mgoltseva@wallarm.com>
2019-11-08 00:26:36 +0300

    test v5.4

--- local.sh
+++ local.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export TOKEN=waf1n4cJsQ7dIL7TthDfMueCe/MCSd7LBhCDgLVXvsO1YZ8w59tJUY6hUQbIXSZE
-
 docker-compose build
-docker-compose up -d dvwa fast 
-docker-compose run --rm -e CI_MODE=testing -e TEST_RUN_URI=http://dvwa:80 -e TEST_RECORD_ID=3219 fast
+docker-compose up -d dvwa selenium fast
+docker-compose run test
+# docker-compose run --rm -e CI_MODE=testing -e TEST_RUN_URI=http://dvwa:80 -e TEST_RECORD_ID=3219 -e WALLARM_API_TOKEN=waf1n4cJsQ7dIL7TthDfMueCe/MCSd7LBhCDgLVXvsO1YZ8w59tJUY6hUQbIXSZE fast
+docker-compose run --rm -e CI_MODE=testing -e TEST_RUN_URI=http://dvwa:80 -e WALLARM_API_TOKEN=4/2EO8Z3L/mnK/fVX0wiQiJfw5rjEAJy6LP5HwDdyPKe25KW7NdHq4R2QRAiaYEO fast
 docker-compose down
